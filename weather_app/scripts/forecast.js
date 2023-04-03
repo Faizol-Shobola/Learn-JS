01a7ac0d49482f483742d035e8f4bd3ac55361ef
Shobola Faizol <shobolafaizol@gmail.com>
2023-04-03 18:19:43 +0100

    Get weather api call

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,1 +1,25 @@
-const key = "6oYUQUGIxe1WAYDBIRsakQ6TGr2c1God";
+const key = "PGLupBbFljnKsHGdNiFSApSn4lwyoOGG";
+
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
